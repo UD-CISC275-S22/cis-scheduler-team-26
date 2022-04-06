ca2cfbbae178ceca4a215584ddde989818403041
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-04-06 12:12:06 -0400

    made a function to add courses and add your inital breadth. Just need to test it now

--- src/Interfaces/course.ts
+++ src/Interfaces/course.ts
@@ -10,3 +10,61 @@ export interface Course {
     /** Whether or not this course has been taken */
     taken: boolean;
 }
+//create empty course list to begin with
+const courseList: Course[] = [];
+/** Add courses to the course list*/
+export function addCourse({
+    id,
+    courseName,
+    numCredits,
+    preReq,
+    taken
+}: Course): void {
+    courseList.push({ id, courseName, numCredits, preReq, taken });
+}
+/**Makes the first semester of classes for you */
+/**The parameters are for adding a breadth that the person
+ * chooses to take for the first semester.*/
+function setUpCourseList(
+    BTid: number,
+    BTcourseName: string,
+    BTnumCredits: number,
+    BTpreReq: string[],
+    BTtaken: boolean
+): void {
+    addCourse({
+        id: 101,
+        courseName: "EGGG",
+        numCredits: 3,
+        preReq: [],
+        taken: false
+    });
+    addCourse({
+        id: 108,
+        courseName: "CISC",
+        numCredits: 3,
+        preReq: [],
+        taken: false
+    });
+    addCourse({
+        id: 241,
+        courseName: "MATH",
+        numCredits: 3,
+        preReq: [],
+        taken: false
+    });
+    addCourse({
+        id: 110,
+        courseName: "ENGL",
+        numCredits: 3,
+        preReq: [],
+        taken: false
+    });
+    addCourse({
+        id: BTid,
+        courseName: BTcourseName,
+        numCredits: BTnumCredits,
+        preReq: BTpreReq,
+        taken: BTtaken
+    });
+}
