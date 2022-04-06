2ba810b1c4e09e0cd5b3247f086c8975c9fc331b
Jack Kingham <wowknead@gmail.com>
2022-04-06 15:32:31 -0400

    Revert "Created a better header and added a small description of what the site will be used for."
    This reverts commit 7c389119287f24b762b552d6b804689f011883ff.
    
    Created the branch on my laptop and committed on my desktop. Sorry everyone :(

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,15 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                University Of Delaware
-                <br />
-                Computer Science Degree Planner
-                <span className="Developer-names">
-                    Developed by: Jack Kingham, Sean Williams, Iclyn Taero
-                </span>
+                UD CISC275 with React Hooks and TypeScript
             </header>
-            <div className="Header-description">
-                Make, manage, and save degree plans for computer science degrees
-                offered at UD
-            </div>
-            <div className="Header-description">
-                View sample degree plans and required courses for multiple
-                degrees
-            </div>
+            <h3>Jack Kingham</h3>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
+                <div>Iclyn Taero</div>
             </p>
+            <div>Developed by: Sean Williams</div>
         </div>
     );
 }
