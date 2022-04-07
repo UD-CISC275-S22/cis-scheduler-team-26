81d5749ed0def3d3832fffb69823b01a34b0c4c8
Sean Williams <sean475@gmail.com>
2022-04-07 12:39:29 -0400

    Commit to pull changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { DegreePlan } from "./Interfaces/degreePlan";
+import { PlanList } from "./PlanList";
+
+const INITIAL_PLANS: DegreePlan[] = [
+    {
+        planName: "Testing Plan",
+        semesterList: [],
+        degree: {
+            title: "Test Degree",
+            requiredCourses: [],
+            requiredCredits: 150
+        },
+        totalCredits: 100
+    }
+];
 
 function App(): JSX.Element {
+    const [plans, setPlans] = useState<DegreePlan[]>([]);
+    const [viewPlan, setViewPlan] = useState<number>(-1);
     return (
         <div className="App">
             <header className="App-header">
@@ -14,6 +31,11 @@ function App(): JSX.Element {
                 <div>Iclyn Taero</div>
             </p>
             <div>Developed by: Sean Williams</div>
+            <PlanList
+                planList={plans}
+                setPlanList={setPlans}
+                setViewPlan={setViewPlan}
+            ></PlanList>
         </div>
     );
 }
--- src/Interfaces/degreePlan.ts
+++ src/Interfaces/degreePlan.ts
@@ -2,8 +2,12 @@ import { Degree } from "./degree";
 import { Semester } from "./semester";
 
 export interface DegreePlan {
+    /** A name for this plan, does not need to be unique */
+    planName: string;
     /** A list of all the semesters in this plan */
     semesterList: Semester[];
     /** The total number of credits required to get this degree */
     degree: Degree;
+
+    totalCredits: number;
 }
