42e717d913744326fdd298615151e87235244a7b
Jack Kingham <wowknead@gmail.com>
2022-04-30 18:38:35 -0400

    Started outlining how a course will be rendered.

--- src/Components/CourseListComponents/RenderCourse.tsx
+++ src/Components/CourseListComponents/RenderCourse.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Course } from "../../Interfaces/course";
 import "./RenderCourse.css";
 
 //Renders a specific course
 export function RenderCourse({ Course }: { Course: Course }): JSX.Element {
+    //Stores the original state of the course for reset button
+    const [originalState] = useState<Course>(Course);
+    //State determining whether to render expanded information
+    const [renderExpanded, setRenderExpanded] = useState<boolean>(false);
     return (
         <div className="courselist-course">
             {Course.courseName + Course.id}
