32f3e24a76683d4cbd2f5924036fa1eda91ebb34
Jack Kingham <wowknead@gmail.com>
2022-04-30 17:27:35 -0400

    Renders a list of requirements for a degree plan. Also tells you if the requirement is fulfilled. Still need to work on styling.

---
+++ src/Components/DegreePlanComponents/ShowDegreeRequirements.tsx
@@ -0,0 +1,58 @@
+import "./ShowDegreeRequirements.css";
+import React from "react";
+import { Course } from "../../Interfaces/course";
+import { Degree } from "../../Interfaces/degree";
+import { Semester } from "../../Interfaces/semester";
+
+export function DegreeRequirements({
+    degree, //degree is the degree to get requirements from
+    semesterList //courses is a list of the
+}: {
+    degree: Degree;
+    semesterList: Semester[];
+}): JSX.Element {
+    /* Checks if a course is in the list
+    I dont use array.includes method because its too sensitive, it returns false unless everything is the same
+    This checks for equality just using the course name and id. */
+    function isCourseInList(course: Course, list: Course[]): boolean {
+        const check: string = course.courseName + course.id.toString();
+        const checklist: string[] = [];
+        list.map((course: Course) =>
+            checklist.push(course.courseName + course.id.toString())
+        );
+        return checklist.includes(check);
+    }
+
+    /* Takes in a list of semesters and returns a list of all courses in the semesters.
+    I do this here so I dont need to do it in the ViewingPlan file. */
+    function getCourses(semesters: Semester[]): Course[] {
+        const courses: Course[] = [];
+        semesters.map((semester: Semester) => {
+            semester.courseList.map((course: Course) => {
+                courses.push(course);
+            });
+        });
+        return courses;
+    }
+    const courses = getCourses(semesterList);
+
+    return (
+        <div className="degree-requirements">
+            <h1>Requirements</h1>
+            <body className="degree-requirements-body">
+                {degree.requiredCourses.map((course: Course) =>
+                    renderCourse(course, isCourseInList(course, courses))
+                )}
+            </body>
+        </div>
+    );
+}
+
+//component to render a single course
+function renderCourse(course: Course, isFulfilled: boolean): JSX.Element {
+    return (
+        <div className="degree-requirements-course">
+            {course.courseName + course.id.toString() + " " + isFulfilled}
+        </div>
+    );
+}
