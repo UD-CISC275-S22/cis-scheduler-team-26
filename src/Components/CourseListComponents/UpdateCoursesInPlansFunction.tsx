16369b480ad5dd9d8966f5f46631fedf3cd5d0aa
Jack Kingham <wowknead@gmail.com>
2022-05-02 12:05:57 -0400

    Changing a course in the course list also changes every occurrence of the course in plans.

--- src/Components/CourseListComponents/UpdateCoursesInPlansFunction.tsx
+++ src/Components/CourseListComponents/UpdateCoursesInPlansFunction.tsx
@@ -6,7 +6,7 @@ import { Semester } from "../../Interfaces/semester";
 
 //iterates through every plan in planlist and every semester in plan and
 //every course in semester to update instances of oldCourse to newCourse
-export function changeCoursesInPlans(
+export function updateCoursesInPlans(
     planlist: DegreePlan[],
     setPlanlist: (d: DegreePlan[]) => void,
     oldCourse: Course,
@@ -17,25 +17,55 @@ export function changeCoursesInPlans(
         const newSemesterList: Semester[] = [];
         plan.semesterList.map((semester: Semester) => {
             const newCourseList: Course[] = [];
+            //populate the new list of courses to construct the new semester with
             semester.courseList.map((course: Course) => {
                 if (areCoursesEqual(oldCourse, course)) {
-                    if (newCourse === null) return;
-                    else newCourseList.push(newCourse);
+                    if (newCourse !== null) newCourseList.push(newCourse);
                 } else newCourseList.push(course);
             });
+            //Construct the new semester and push it to the back of the new semester list
             newSemesterList.push({
                 year: semester.year,
                 season: semester.season,
                 courseList: newCourseList,
-                totalCredits: 0 //THIS MUST BE CHANGED, CALCULATE THE NEW NUMBER OF CREDITS
+                totalCredits: getTotalCreditsCourselist(newCourseList)
             });
         });
+        newPlanlist.push({
+            planName: plan.planName,
+            semesterList: newSemesterList,
+            degree: plan.degree,
+            totalCredits: getTotalCreditsSemesterList(newSemesterList),
+            isSaved: plan.isSaved
+        });
     });
+    setPlanlist(newPlanlist);
     return;
 }
 
+//check if courses are equal by their department and id
 function areCoursesEqual(c1: Course, c2: Course): boolean {
     return (
         c1.courseName + c1.id.toString() === c2.courseName + c2.id.toString()
     );
+}
+
+//canculate the total number of credits over a course list
+function getTotalCreditsCourselist(courseList: Course[]): number {
+    const numList: number[] = [];
+    courseList.map((course: Course) => numList.push(course.numCredits));
+    const sum = 0;
+    numList.reduce((prev, curr) => prev + curr, sum);
+    return sum;
+}
+
+//calculate the total number of credits over a semester list
+function getTotalCreditsSemesterList(semesterlist: Semester[]): number {
+    const numList: number[] = [];
+    semesterlist.map((semester: Semester) =>
+        numList.push(semester.totalCredits)
+    );
+    const sum = 0;
+    numList.reduce((prev, curr) => prev + curr, sum);
+    return sum;
 }
