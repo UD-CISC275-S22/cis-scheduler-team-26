dd0bc294e2e92398cc793fcbb2d8ff0aaff2af0f
Sean Williams <sean475@gmail.com>
2022-05-02 20:35:41 -0400

    Fixed current tests to work with style changes

--- src/Components/DegreePlanComponents/ViewingPlan.test.tsx
+++ src/Components/DegreePlanComponents/ViewingPlan.test.tsx
@@ -15,16 +15,16 @@ describe("ViewingPlan Tests", () => {
         expect(screen.getByText(/Winter 2022/i)).toBeInTheDocument();
         expect(screen.getByText(/Spring 2022/i)).toBeInTheDocument();
     });
-    test("All 4 inital test courses are displayed for both semesters", () => {
-        expect(screen.getAllByText(/101/i).length === 2);
-        expect(screen.getAllByText(/EGGG/i).length === 2);
+    test("All 4 inital test courses are displayed", () => {
+        expect(screen.getByText(/101/i)).toBeInTheDocument();
+        expect(screen.getByText(/EGGG/i)).toBeInTheDocument();
         expect(screen.getAllByText(/108/i).length === 2);
         expect(screen.getAllByText(/CISC/i).length === 2);
         expect(screen.getAllByText(/241/i).length === 2);
         expect(screen.getAllByText(/MATH/i).length === 2);
-        expect(screen.getAllByText(/110/i).length === 1);
-        expect(screen.getAllByText(/ENGL/i).length === 1);
-        expect(screen.getAllByText(/3/i).length === 7);
+        expect(screen.getAllByText(/110/i).length === 2);
+        expect(screen.getAllByText(/ENGL/i).length === 2);
+        expect(screen.getAllByText(/3/i).length === 4);
     });
     test("Courses can be removed", () => {
         const editButton = screen.getAllByRole("button", {
@@ -35,23 +35,31 @@ describe("ViewingPlan Tests", () => {
             name: /Remove Course/i
         });
         removeCourseButton[0].click();
-        expect(screen.getByText(/EGGG/i)).toBeInTheDocument();
-        expect(screen.getAllByText(/3/i).length === 6);
+        expect(screen.queryByText(/EGGG/i)).not.toBeInTheDocument();
+        expect(screen.getAllByText(/3/i).length === 4);
     });
     test("Courses can be added", () => {
         const editButton = screen.getAllByRole("button", {
             name: /Edit Courses/i
         });
         editButton[0].click();
+        const removeCourseButton = screen.getAllByRole("button", {
+            name: /Remove Course/i
+        });
+        removeCourseButton[0].click();
+        expect(screen.queryByText(/EGGG/i)).not.toBeInTheDocument();
+        expect(screen.getAllByText(/3/i).length === 4);
+        editButton[0].click();
         const typeDropdown = screen.getByRole("combobox", {
             name: /Pick the Course to be Added:/i
         });
-        userEvent.selectOptions(typeDropdown, "ENGL110");
+        userEvent.selectOptions(typeDropdown, "EGGG101");
         const addButton = screen.getByRole("button", {
             name: /Add Course/i
         });
         addButton.click();
-        expect(screen.getAllByText(/ENGL/i).length === 2);
+        expect(screen.getByText(/101/i)).toBeInTheDocument();
+        expect(screen.getByText(/EGGG/i)).toBeInTheDocument();
     });
     test("Semesters can be removed", () => {
         const editButton = screen.getByRole("button", {
@@ -62,45 +70,40 @@ describe("ViewingPlan Tests", () => {
             name: /Delete Semester/i
         });
         removeSemesterButton[0].click();
-        expect(screen.getByText(/EGGG/i)).toBeInTheDocument();
+        expect(screen.queryByText(/EGGG/i)).not.toBeInTheDocument();
         expect(screen.queryByText(/Winter 2022/i)).not.toBeInTheDocument();
-        expect(screen.getAllByText(/3/i).length === 4);
+        expect(screen.getAllByText(/3/i).length === 1);
     });
     test("Semesters can be added", () => {
         const editButton = screen.getByRole("button", {
             name: /Edit Semesters/i
         });
         editButton.click();
+        const addButton = screen.getByRole("button", {
+            name: /Add Semester/i
+        });
+        addButton.click();
         const seasonDropdown = screen.getByRole("combobox", {
             name: /Season:/i
         });
         userEvent.selectOptions(seasonDropdown, "Summer");
-        const addSemesterButton = screen.getByRole("button", {
+        const addSemesterButton = screen.getAllByRole("button", {
             name: /Add Semester/i
         });
-        addSemesterButton.click();
+        addSemesterButton[1].click();
+        const cancelButton = screen.getByRole("button", {
+            name: /Cancel/i
+        });
+        cancelButton.click();
         expect(screen.getByText(/Summer 2022/i)).toBeInTheDocument();
     });
     test("The initial credits in the testing plan are displayed", () => {
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
         expect(
-            screen.getByText(/Currently Have 21 out of 150 required Credits/i)
-        ).toBeInTheDocument();
-    });
-    test("When adding a course, the total credits in updated", () => {
-        const editButton = screen.getAllByRole("button", {
-            name: /Edit Courses/i
-        });
-        editButton[0].click();
-        const typeDropdown = screen.getByRole("combobox", {
-            name: /Pick the Course to be Added:/i
-        });
-        userEvent.selectOptions(typeDropdown, "ENGL110");
-        const addButton = screen.getByRole("button", {
-            name: /Add Course/i
-        });
-        addButton.click();
-        expect(
-            screen.getByText(/Currently Have 24 out of 150 required Credits/i)
+            screen.getByText(/Completed 12 out of 124 required credits/i)
         ).toBeInTheDocument();
     });
     test("When removing a course, the score is updated", () => {
@@ -112,8 +115,48 @@ describe("ViewingPlan Tests", () => {
             name: /Remove Course/i
         });
         removeCourseButton[0].click();
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
         expect(
-            screen.getByText(/Currently Have 18 out of 150 required Credits/i)
+            screen.getByText(/Completed 9 out of 124 required credits/i)
+        ).toBeInTheDocument();
+    });
+    test("When adding a course, the total credits in updated", () => {
+        const editButton = screen.getAllByTestId("editCourseButton");
+        editButton[0].click();
+        const removeCourseButton = screen.getAllByRole("button", {
+            name: /Remove Course/i
+        });
+        removeCourseButton[0].click();
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
+        expect(
+            screen.getByText(/Completed 9 out of 124 required credits/i)
+        ).toBeInTheDocument();
+        const viewPlanButton = screen.getAllByRole("button", {
+            name: "View/Edit Plan"
+        });
+        viewPlanButton[0].click();
+        const editButton2 = screen.getAllByTestId("editCourseButton");
+        editButton2[0].click();
+        const typeDropdown = screen.getByRole("combobox", {
+            name: /Pick the Course to be Added:/i
+        });
+        userEvent.selectOptions(typeDropdown, "EGGG101");
+        const addButton = screen.getByRole("button", {
+            name: /Add Course/i
+        });
+        addButton.click();
+        const returnButton2 = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton2.click();
+        expect(
+            screen.getByText(/Completed 12 out of 124 required credits/i)
         ).toBeInTheDocument();
     });
     test("Semesters can be removed", () => {
@@ -125,8 +168,49 @@ describe("ViewingPlan Tests", () => {
             name: /Delete Semester/i
         });
         removeSemesterButton[0].click();
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
         expect(
-            screen.getByText(/Currently Have 12 out of 150 required Credits/i)
+            screen.getByText(/Completed 3 out of 124 required credits/i)
+        ).toBeInTheDocument();
+    });
+    test("Semesters can be cleared individually", () => {
+        const editButton = screen.getAllByRole("button", {
+            name: /Edit Courses/i
+        });
+        editButton[0].click();
+        const clearButton = screen.getByRole("button", {
+            name: /Clear All Courses/i
+        });
+        clearButton.click();
+        expect(screen.getByText(/108/i)).toBeInTheDocument();
+        expect(screen.queryByText(/101/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/EGGG/i)).not.toBeInTheDocument();
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
+        expect(
+            screen.getByText(/Completed 3 out of 124 required credits/i)
+        ).toBeInTheDocument();
+    });
+    test("All Semesters can be cleared", () => {
+        const clearButton = screen.getByRole("button", {
+            name: /Clear All Semesters/i
+        });
+        clearButton.click();
+        expect(screen.getByText(/108/i)).toBeInTheDocument();
+        expect(screen.queryByText(/101/i)).not.toBeInTheDocument();
+        expect(screen.queryByText(/EGGG/i)).not.toBeInTheDocument();
+        expect(screen.getByText(/ENGL110/i)).toBeInTheDocument();
+        const returnButton = screen.getByRole("button", {
+            name: /Return to Plan List/i
+        });
+        returnButton.click();
+        expect(
+            screen.getByText(/Completed 0 out of 124 required credits/i)
         ).toBeInTheDocument();
     });
 });
